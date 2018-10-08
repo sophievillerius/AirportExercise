9869a6f6c90ea5083ec700770ae72766ae2554d9
sophievillerius <sophievillerius@gmail.com>
2018-10-08 09:17:55 +0200

    Initial commit

---
+++ src/main/java/com/capgemini/AirportExercise/AirportExerciseApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini.AirportExercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirportExerciseApplication.class, args);
+	}
+}
